b6f51fca286939edfc3b0226062916a7b886b2a0
zayne017 <981632961@qq.com>
2025-11-20 21:18:33 +0800

    Delete lab4/obj directory

--- lab4/obj/kern/driver/dtb.d
+++
@@ -1,3 +0,0 @@
-obj/kern/driver/dtb.o obj/kern/driver/dtb.d: kern/driver/dtb.c \
- kern/driver/dtb.h libs/defs.h libs/stdio.h libs/stdarg.h libs/string.h \
- kern/mm/memlayout.h libs/atomic.h libs/list.h
